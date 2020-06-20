4a1275b88824c5591be9a4ff36d8461b7aac859f
David Ezequiel Assen <dassen@solstice.com>
2020-06-20 19:25:54 -0300

    Added home endpoint

--- controller/core.controller.js
+++ controller/core.controller.js
@@ -1,15 +1,23 @@
-const httpError = require('http-errors');
+const httpError = require("http-errors");
 
 exports.health = (req, res, next) => {
-    const healthCheck = {
-        status: 'UP', 
-        timestamp: Date.now(), 
-        uptime: process.uptime()
-    };
+  const healthCheck = {
+    status: "UP",
+    timestamp: Date.now(),
+    uptime: process.uptime(),
+  };
 
-    try {        
-        res.status(200).json(healthCheck);
-    } catch(err) {
-        next(httpError(503, err));
-    }
+  try {
+    res.status(200).json(healthCheck);
+  } catch (err) {
+    next(httpError(503, err));
+  }
+};
+
+exports.api = (req, res, next) => {
+  try {
+    res.status(200).json({ status: "ok!" });
+  } catch (err) {
+    next(httpError(503, err));
+  }
 };
--- routes/core.route.js
+++ routes/core.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const coreController = require('../controller/core.controller');
 
+router.get('/', coreController.api);
 router.get('/health', coreController.health);
 
 module.exports = router;
